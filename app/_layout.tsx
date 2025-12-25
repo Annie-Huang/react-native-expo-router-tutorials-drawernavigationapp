cb1dc34430b1dddc7508f1c540190a1f3129ae7c
Annie Huang <annie.hn@gmail.com>
2025-12-25 15:14:21 +1100

    Use <GestureHandlerRootView> so we can slide out the page from the left side.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
 import { Drawer } from 'expo-router/drawer';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function RootLayout() {
-  return <Drawer />;
+  // Wrap with <GestureHandlerRootView> so we can click the hamburger menu or use slider to draw the the drawer from the left hand side of the page out.
+  // Pretty much like Gmail
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer />
+    </GestureHandlerRootView>
+  );
 }
