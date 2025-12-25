c6290832adbd755cb8cd2c8b717407d90acd87f2
Annie Huang <annie.hn@gmail.com>
2025-12-25 14:59:28 +1100

    Create the app/dashboard.tsx page.

---
+++ app/dashboard.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Text, View, StyleSheet } from 'react-native';
+
+const Dashboard = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Dashboard page</Text>
+    </View>
+  );
+};
+
+export default Dashboard;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
