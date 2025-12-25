b1f02a0fef12eaa0b740365887a77bd6d0e16417
Annie Huang <annie.hn@gmail.com>
2025-12-25 15:23:40 +1100

    Add different icon with the page link in the draw with 'drawerIcon' option

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Drawer } from 'expo-router/drawer';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function RootLayout() {
   // Wrap with <GestureHandlerRootView> so we can click the hamburger menu or use slider to draw the the drawer from the left hand side of the page out.
@@ -9,15 +10,33 @@ export default function RootLayout() {
       <Drawer>
         <Drawer.Screen
           name='index'
-          options={{ title: 'Home title', drawerLabel: 'Home label' }}
+          options={{
+            title: 'Home title',
+            drawerLabel: 'Home label', // Change the label display inside the drawer page.
+            drawerIcon: ({ color }) => (
+              <FontAwesome name='home' size={24} color={color} />
+            ),
+          }}
         />
         <Drawer.Screen
           name='dashboard'
-          options={{ title: 'Dashboard title', drawerLabel: 'Dashboard label' }}
+          options={{
+            title: 'Dashboard title',
+            drawerLabel: 'Dashboard label',
+            drawerIcon: ({ color }) => (
+              <FontAwesome name='dashboard' size={24} color={color} />
+            ),
+          }}
         />
         <Drawer.Screen
           name='settings'
-          options={{ title: 'Settings title', drawerLabel: 'Settings label' }}
+          options={{
+            title: 'Settings title',
+            drawerLabel: 'Settings label',
+            drawerIcon: ({ color }) => (
+              <FontAwesome name='cog' size={24} color={color} />
+            ),
+          }}
         />
       </Drawer>
     </GestureHandlerRootView>
